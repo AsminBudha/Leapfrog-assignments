f30c27c288876f93333fc6c16c7ae5558623770d
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2020-06-12 00:19:18 +0545

    Fix: path to git & github repo

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ const data = [
     id: 2,
     title: 'Git & GitHub',
     sourceCode: 'https://github.com/prashantacharya/Leapfrog-assignments',
-    liveDemo: '/',
+    liveDemo: '#',
     type: 'github-alt',
   },
 ]
