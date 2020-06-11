0256c934fb5016a7b2eeb2a7c1891b23d5b7b3a8
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2020-06-11 20:09:37 +0545

    Feat: Fill table from data

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    id: 1,
+    title: 'Card Design',
+    sourceCode:
+      'https://github.com/prashantacharya/Leapfrog-assignments/tree/master/design/card-design',
+    liveDemo: './design/card-design',
+    type: 'html5',
+  },
+  {
+    id: 2,
+    title: 'Git & GitHub',
+    sourceCode: 'https://github.com/prashantacharya/Leapfrog-assignments',
+    liveDemo: '/',
+    type: 'github-alt',
+  },
+]
